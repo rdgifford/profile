83e70760d928067cafe8a5adea1fed624e45c975
rdgifford <robbie.gifford@gmail.com>
2017-03-10 13:19:27 -0800

    Add express server to run React

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-var React = require('react');
+// var React = require('react');
 
-class Pane extends React.component {
-    render() {
-        return <h1>Hello</h1>;
-    }
-}
+// class Pane extends React.component {
+//     render() {
+//         return <h1>Hello</h1>;
+//     }
+// }
 
-
+console.log('Hello world');
