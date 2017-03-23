3f080ab2c01f9496e472526eea9990e804231adb
rdgifford <robbie.gifford@gmail.com>
2017-03-23 15:35:58 -0700

    Implement initial vertical infinite scroll with dummy data using client height, scroll top, and scroll height from onscroll event target

--- src/main.js
+++ src/main.js
@@ -1,23 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
+import _ from 'lodash';
+
 // if bundle.js already exists starting server uses bundle.js from file instead of hot compiled,
 // why this behavior exists or how to turn it off, I don't know. Deleting bundle.js before starting server
 // works though.
 
-class InfiniteScroll extends React.Component {
+class VerticalInfiniteScroll extends React.Component {
     constructor() {
         super()
+        this.state = {
+            items: _.fill(Array(50), 'HOWDY'),
+        }
     };
+    updateItems(diff) {
+        if(Array.isArray(diff)) {
+            this.setState({
+                items: this.state.items.concat(diff)
+            })
+        } else {
+            throw new TypeError('updateItems should only be given an Array');
+        }
+    }
     render() {
         return (
-            <div className='scroll'>hello</div>
+            <div className='scroll' onScroll={
+                event => {
+                    let t = event.target;
+                    // scroll remaining as percentage of scroll bar left
+                    let scrollRemaining = 1 - (t.scrollTop / (t.scrollHeight - t.clientHeight));
+                    if(scrollRemaining <= .25) {
+                        this.updateItems(_.fill(Array(50), "YOYO"))
+                    }
+                }
+            }>{
+                this.state.items.map((item, i) => {
+                    return <div className='scrollItem' key={i}>{item}</div>;
+                })
+            }</div>
         );
     }
 }
 $().ready(() => {
-    console.log('DOM ready');
-    let body = document.getElementsByClassName('container')[0];
-    console.log(body);
-    ReactDOM.render(<InfiniteScroll />, body);
+    let container = document.getElementsByClassName('container')[0];
+    ReactDOM.render(<VerticalInfiniteScroll />, container);
 });
